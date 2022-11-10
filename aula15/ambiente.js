8da0b51fcb5394f75095cafe4c4b84c12b24adf5
SanchesVitor <112482028+SanchesVitor@users.noreply.github.com>
2022-11-10 14:18:37 -0300

    atualização aula15 (vetor)

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -4,3 +4,10 @@ num.sort()
 console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+let pos = num.indexOf(1)
+if (pos == -1) {
+    console.log('O valor não foi encontrado!')
+} else {
+    console.log(`O valor ${num[pos]} está na posição ${pos}`)
+}
